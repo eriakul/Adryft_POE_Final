de36126bab4825fc13841db84f96c415f2c7358d
Erika Lu <erika.lu@students.olin.edu>
2018-10-26 14:26:26 -0400

    add string by click function

--- Computer Vision/simulation.py
+++ Computer Vision/simulation.py
@@ -1,15 +1,22 @@
 import pygame
-from math import floor, pi, cos, sin
+from math import floor, pi, cos, sin, hypot
+from time import sleep
+from operator import itemgetter
+
 class System:
-    def __init__(self, window_size, peg_num = 36, string_thickness = 1, peg_size = 2):
+    def __init__(self, window_size, peg_num = 36, string_thickness = 1, peg_size = 5):
         self.peg_num = peg_num
-        self.string_thickness = string_thickness
-        self.peg_size = peg_size
+
         self.screen_properties = dict(
             board_color = pygame.Color(255, 255, 255, 255),
             board_radius = floor(window_size[1]/2*.9),
             center = [floor(window_size[0]/2), floor(window_size[1]/2)],
-            pegs = self.create_pegs()
+            pegs = [],
+            peg_size = peg_size,
+            peg_color = pygame.Color(100, 100, 100, 255),
+            peg_highlight = pygame.Color(100, 255, 255, 255),
+            string_color = pygame.Color(0, 0, 0, 255),
+            string_thickness = string_thickness
             )
 
         self.screen = pygame.display.set_mode(window_size)
@@ -19,6 +26,11 @@ class System:
                             self.screen_properties["center"],
                             self.screen_properties["board_radius"])
         pygame.display.flip()
+        self.create_pegs()
+        self.current_peg = self.screen_properties["pegs"][0]
+        self.refresh_pegs()
+
+
 
     def update_window(self):
         pass
@@ -26,20 +38,49 @@ class System:
     def create_pegs(self):
         angle_steps = 2*pi/self.peg_num
         peg_locations = []
-        # for i in range(0, 2*pi, angle_steps):
-        #     location = []
-        #     peg_locations.append()
-        return angle_steps
+        radius = self.screen_properties["board_radius"]
+        center_x = self.screen_properties["center"][0]
+        center_y = self.screen_properties["center"][1]
+        for i in range(self.peg_num):
+            theta = 3/2*pi +i*angle_steps
+            x_delta = radius*cos(theta)
+            y_delta = radius*sin(theta)
+            location = [floor(center_x + x_delta), floor(center_y + y_delta)]
+            peg_locations.append(location)
 
+        self.screen_properties["pegs"] = peg_locations
+
+    def refresh_pegs(self):
+        for location in self.screen_properties["pegs"]:
+            pygame.draw.circle(self.screen,
+                                self.screen_properties["peg_color"],
+                                location,
+                                self.screen_properties["peg_size"])
+        #highlight current peg
+        pygame.draw.circle(self.screen,
+                            self.screen_properties["peg_highlight"],
+                            self.current_peg,
+                            self.screen_properties["peg_size"])
+        pygame.display.flip()
+    def process_click(self):
+        x, y = event.pos
+        last_peg = self.current_peg
+        closest_peg = min(self.screen_properties["pegs"],
+        key = lambda pos: hypot(pos[0]-x, pos[1]-y))
+
+        pygame.draw.line(self.screen, self.screen_properties["string_color"],
+                        last_peg, closest_peg, self.screen_properties["string_thickness"])
+
+        self.current_peg = closest_peg
+        self.refresh_pegs()
 
 
 if __name__ == "__main__":
 
     window_size = [1000, 500]
 
-
     pygame.init()
-    hello = System(window_size)
+    hello = System(window_size, peg_num = 36)
     done = False
 
     while not done:
@@ -49,3 +90,6 @@ if __name__ == "__main__":
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     done = True
+            if event.type == pygame.MOUSEBUTTONDOWN :
+                hello.process_click()
+        sleep(.05)
