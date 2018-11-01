cff91e9c5e2b3cb3a6e0513ba31e32a6e57156be
Enmo Ren <eren@olin.edu>
2018-11-01 16:47:55 -0400

    Tested stepper motor with arduino mega

---
+++ POE_1_Stepper_test/POE_1_Stepper_test.ino
@@ -0,0 +1,52 @@
+#include <SpeedyStepper.h>
+
+// Create AccelStepper object for stepper driver with Step and Direction pins
+const int MOTOR_STEP_PIN_X = 54;
+const int MOTOR_DIRECTION_PIN_X = 55;
+const int STEPPER_ENABLE_PIN_X = 38;
+const int MOTOR_STEP_PIN_Y = 60;
+const int MOTOR_DIRECTION_PIN_Y = 61;
+const int STEPPER_ENABLE_PIN_Y = 56;
+
+SpeedyStepper Xaxis; // pin 54 = step, pin 55 = direction
+SpeedyStepper Yaxis; // pin 60 = step, pin 61 = direction 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+  
+  // set stepper motor speeds
+  pinMode(STEPPER_ENABLE_PIN_X, OUTPUT);
+  digitalWrite(STEPPER_ENABLE_PIN_X,LOW);
+  pinMode(STEPPER_ENABLE_PIN_Y, OUTPUT);
+  digitalWrite(STEPPER_ENABLE_PIN_Y,LOW);
+  Xaxis.connectToPins(MOTOR_STEP_PIN_X, MOTOR_DIRECTION_PIN_X);
+  Yaxis.connectToPins(MOTOR_STEP_PIN_Y, MOTOR_DIRECTION_PIN_Y);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  Xaxis.setSpeedInStepsPerSecond(100);
+//  Xaxis.setAccelerationInStepsPerSecondPerSecond(100);
+//  Xaxis.moveRelativeInSteps(-200);
+  
+  Xaxis.setStepsPerRevolution(3200);
+  Xaxis.setSpeedInRevolutionsPerSecond(1);
+  Xaxis.setAccelerationInRevolutionsPerSecondPerSecond(1);
+  Xaxis.setupRelativeMoveInRevolutions(1);
+//  Xaxis.moveRelativeInRevolutions(1);
+  
+  Yaxis.setStepsPerRevolution(3200);
+  Yaxis.setSpeedInRevolutionsPerSecond(1);
+  Yaxis.setAccelerationInRevolutionsPerSecondPerSecond(1);
+  Yaxis.setupRelativeMoveInRevolutions(1);
+//  Yaxis.moveRelativeInRevolutions(1);
+//  delay(500);
+//  Xaxis.moveRelativeInRevolutions(-1.5);
+    while((!Yaxis.motionComplete()) || (!Xaxis.motionComplete()))
+    {
+      Yaxis.processMovement();
+      Xaxis.processMovement();
+    }
+}
--- POE_1stepper_test/POE_1stepper_test.ino
+++ POE_1stepper_test/POE_1stepper_test.ino
@@ -1,25 +1,25 @@
-//#include <AccelStepper.h>
+#include <AccelStepper.h>
 //#include <SpeedyStepper.h>
-//
-//// Create AccelStepper object for stepper driver with Step and Direction pins
-//AccelStepper Xaxis(1, 54, 55); // pin 54 = step, pin 55 = direction
+
+// Create AccelStepper object for stepper driver with Step and Direction pins
+AccelStepper Xaxis(1, 54, 55); // pin 54 = step, pin 55 = direction
 //AccelStepper Yaxis(1, 60, 61); // pin 60 = step, pin 61 = direction
-//
-//void setup() {
-//  // put your setup code here, to run once:
-//  Serial.begin(9600);           // set up Serial library at 9600 bps
-//  Serial.println("Stepper test!");
-//  // set stepper motor speeds
-//  Xaxis.setSpeed(50);
-//  Xaxis.moveTo(100); 
-//  //Yaxis.setSpeed(50);
-//  //Yaxis.moveTo(100); 
-//}
-//
-//void loop() {
-//  // put your main code here, to run repeatedly:
-//  Serial.println(Xaxis.distanceToGo());
-//  Xaxis.runSpeed();
-//  //Serial.println(Yaxis.distanceToGo());
-//  //Yaxis.runSpeed();
-//}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+  // set stepper motor speeds
+  Xaxis.setSpeed(50);
+  Xaxis.moveTo(100); 
+  //Yaxis.setSpeed(50);
+  //Yaxis.moveTo(100); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(Xaxis.distanceToGo());
+  Xaxis.runSpeed();
+  //Serial.println(Yaxis.distanceToGo());
+  //Yaxis.runSpeed();
+}
